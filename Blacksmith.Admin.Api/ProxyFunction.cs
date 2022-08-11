da96d34b97233d995d1c510b506179f40589d863
Ashan Perera <ashan@blockchainlabs.ai>
2022-08-11 15:23:33 +0700

    initial commit

---
+++ Blacksmith.Admin.Api/ProxyFunction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using BlockchainLabs.Common.Authorization.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Blacksmith.Admin.Api
+{
+    public class ProxyFunction
+    {
+
+        private readonly IAccessTokenProvider _accessTokenProvider;
+        public ProxyFunction(IAccessTokenProvider accessTokenProvider)
+        {
+            _accessTokenProvider = accessTokenProvider;
+        }
+    
+        [FunctionName(nameof(Proxy))]
+        public async Task<IActionResult> Proxy(
+            [HttpTrigger(AuthorizationLevel.Anonymous, nameof(HttpMethods.Get), nameof(HttpMethods.Post), nameof(HttpMethods.Patch), Route = "{*test}")]
+            HttpRequest req, ILogger log)
+        {
+            var principal = _accessTokenProvider.GetAccessToken(req);
+            var route = req.Path.ToString();
+            var results = route.Split('/');
+
+            var proxyPrefix = results[0];
+            var api = results[1];
+            principal.Principal.Claims.
+            
+            
+            
+                
+            var method = req.Method;
+            Console.WriteLine($"{method} - {route}");
+            Console.WriteLine(string.Join(',', results));
+            return new OkObjectResult(route);
+        }
+    }
+}
---
+++ Blacksmith.Admin.Api/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Blacksmith.Admin.Api;
+using BlockchainLabs.Common;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace Blacksmith.Admin.Api
+{
+    internal class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.AddJwtConfiguration();
+        }
+    }
+}
